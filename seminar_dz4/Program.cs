dcb916ef13dbfe2d35f63405685d0108950278f0
vjlion <89218741998@mail.ru>
2022-12-20 21:00:24 +0300

    seminar_dz1

---
+++ seminar_dz4/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+Console.Write("Введите 1-ю цифру: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ю цифру: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ю цифру: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine($"Максимальное число: {n1}");
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine($"Максимальное число: {n2}");
+} 
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine($"Максимальное число: {n3}");
+}
+else
+{
+    Console.WriteLine("Цифры равны");
+}
---
+++ seminar_dz8/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (n >= count)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+    }
+    count++;
+}
